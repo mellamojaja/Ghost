ca953461622267b56390e31098b0762359032675
mellamojaja <jaja.granada@gmail.com>
2018-02-15 00:25:28 +0100

    Async connection to give help to the player as he types

--- Ghost.MVC/Scripts/ghostutilities.js
+++ Ghost.MVC/Scripts/ghostutilities.js
@@ -1,7 +1,58 @@
 ﻿$(document).ready(function () {
-    var url = 'http://localhost:50519/api/Analyse';
-    
-    $.post(url, { Word: "hola" }, function (data) {
-        console.log(data);
-    })
-});
+
+    document.ghostutilities = 
+
+    function setVisibilityOfHumanPlayerHelp() {
+        var $humanPlayerHelp = $('#humanPlayerHelp'),
+            $showHumanPlayerHelp = $('#showHumanPlayerHelp');
+
+        if ($showHumanPlayerHelp.is(':checked') !== $humanPlayerHelp.is(':visible')) {
+            if ($showHumanPlayerHelp.val()) {
+                $humanPlayerHelp.addClass('visible');
+            }
+            else {
+                $humanPlayerHelp.addClass('invisible');
+            }
+        }
+    }
+
+    function getWordPlusNewMove() {
+        var newMoveValue = $('#newMove').val().trim(),
+            wordAreaText = $('#wordArea').text().trim();
+
+        if ((newMoveValue.length > 0) && (newMoveValue[0] !== '')) {
+            return wordAreaText + newMoveValue[0].substr(0, 1);
+        } else {
+            return wordAreaText;
+        }
+    }
+
+    function getStateAnalysis(callback) {
+        var url = 'http://localhost:50519/api/Analyse',
+            word = getWordPlusNewMove();
+
+        $.post(url, { Word: word }, callback);
+    }
+
+    function buildAndSetHumanPlayerHelpMessage(analysis) {
+        var $humanPlayerHelp = $('#humanPlayerHelp'),
+            re = new RegExp('Player 0', 'i'),
+            word = getWordPlusNewMove(),
+            msg;
+
+        msg = 'For word "' + word + '", ' + analysis.Help;
+        msg = msg.replace(re, 'you');
+        $humanPlayerHelp.text(msg);
+    }
+
+    //$(document).ready(function () {
+    //    var $showHumanPlayerHelp = $('#showHumanPlayerHelp'),
+    //        $newMove = $('#newMove');
+
+    //    $showHumanPlayerHelp.change(setVisibilityOfHumanPlayerHelp);
+
+    //    $newMove.change(function () {
+    //        getStateAnalysis(buildAndSetHumanPlayerHelpMessage);
+    //    })
+    //});
+}); 
