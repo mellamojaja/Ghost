952a7d73832b75595a35e21e1ec866832a547b0d
Juan Alfredo de Jeronimo Alvarez <jaja.granada@gmail.com>
2018-02-15 09:14:06 +0100

    New javascript library approach

--- Ghost.MVC/Scripts/ghostutilities.js
+++ Ghost.MVC/Scripts/ghostutilities.js
@@ -1,58 +1,61 @@
-﻿$(document).ready(function () {
+﻿Ghost = Ghost || {};
 
-    document.ghostutilities = 
+Ghost.Play = (function () {
+    
+        function setVisibilityOfHumanPlayerHelp() {
+            var $humanPlayerHelp = $('#humanPlayerHelp'),
+                $showHumanPlayerHelp = $('#showHumanPlayerHelp');
 
-    function setVisibilityOfHumanPlayerHelp() {
-        var $humanPlayerHelp = $('#humanPlayerHelp'),
-            $showHumanPlayerHelp = $('#showHumanPlayerHelp');
-
-        if ($showHumanPlayerHelp.is(':checked') !== $humanPlayerHelp.is(':visible')) {
-            if ($showHumanPlayerHelp.val()) {
-                $humanPlayerHelp.addClass('visible');
-            }
-            else {
-                $humanPlayerHelp.addClass('invisible');
+            if ($showHumanPlayerHelp.is(':checked') !== $humanPlayerHelp.is(':visible')) {
+                if ($showHumanPlayerHelp.val()) {
+                    $humanPlayerHelp.addClass('visible');
+                }
+                else {
+                    $humanPlayerHelp.addClass('invisible');
+                }
             }
         }
-    }
 
-    function getWordPlusNewMove() {
-        var newMoveValue = $('#newMove').val().trim(),
-            wordAreaText = $('#wordArea').text().trim();
+        function getWordPlusNewMove() {
+            var newMoveValue = $('#newMove').val().trim(),
+                wordAreaText = $('#wordArea').text().trim();
 
-        if ((newMoveValue.length > 0) && (newMoveValue[0] !== '')) {
-            return wordAreaText + newMoveValue[0].substr(0, 1);
-        } else {
-            return wordAreaText;
+            if ((newMoveValue.length > 0) && (newMoveValue[0] !== '')) {
+                return wordAreaText + newMoveValue[0].substr(0, 1);
+            } else {
+                return wordAreaText;
+            }
         }
-    }
 
-    function getStateAnalysis(callback) {
-        var url = 'http://localhost:50519/api/Analyse',
-            word = getWordPlusNewMove();
+        function getStateAnalysis(callback) {
+            var url = 'http://localhost:50519/api/Analyse',
+                word = getWordPlusNewMove();
 
-        $.post(url, { Word: word }, callback);
-    }
+            $.post(url, { Word: word }, callback);
+        }
 
-    function buildAndSetHumanPlayerHelpMessage(analysis) {
-        var $humanPlayerHelp = $('#humanPlayerHelp'),
-            re = new RegExp('Player 0', 'i'),
-            word = getWordPlusNewMove(),
-            msg;
+        function buildAndSetHumanPlayerHelpMessage(analysis) {
+            var $humanPlayerHelp = $('#humanPlayerHelp'),
+                re = new RegExp('Player 0', 'i'),
+                word = getWordPlusNewMove(),
+                msg;
 
-        msg = 'For word "' + word + '", ' + analysis.Help;
-        msg = msg.replace(re, 'you');
-        $humanPlayerHelp.text(msg);
-    }
+            msg = 'For word "' + word + '", ' + analysis.Help;
+            msg = msg.replace(re, 'you');
+            $humanPlayerHelp.text(msg);
+        }
 
-    //$(document).ready(function () {
-    //    var $showHumanPlayerHelp = $('#showHumanPlayerHelp'),
-    //        $newMove = $('#newMove');
+        (function ($) {
+            $(document).ready(function () {
+                var $showHumanPlayerHelp = $('#showHumanPlayerHelp'),
+                    $newMove = $('#newMove');
 
-    //    $showHumanPlayerHelp.change(setVisibilityOfHumanPlayerHelp);
+                $showHumanPlayerHelp.change(setVisibilityOfHumanPlayerHelp);
 
-    //    $newMove.change(function () {
-    //        getStateAnalysis(buildAndSetHumanPlayerHelpMessage);
-    //    })
-    //});
-}); 
+                $newMove.change(function () {
+                    getStateAnalysis(buildAndSetHumanPlayerHelpMessage);
+                })
+            });
+        })($);
+    
+})(); 
