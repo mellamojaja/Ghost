5fbea7a22996f9d31ab69d01120d9be64204c997
Juan Alfredo de Jeronimo Alvarez <jaja.granada@gmail.com>
2018-02-15 09:13:42 +0100

    Added more unit tests to the Controller

--- Ghost.AI.API.Tests/AnalyseControllerTests.cs
+++ Ghost.AI.API.Tests/AnalyseControllerTests.cs
@@ -26,10 +26,54 @@ namespace Ghost.AI.API.Tests
         }
 
         [TestMethod]
-        public void Get_WinnerPropertyMustBeFilled()
-        {            
+        public void Analize_AllFieldsMustBeDefined()
+        {
+            State = new GameStateModel("hello");
             var analysis = Controller.Post(State);
+            Assert.IsNotNull(analysis.HasWinner);
+            Assert.IsNotNull(analysis.Winner);
+            Assert.IsNotNull(analysis.ExpectedWinner);
+            Assert.IsNotNull(analysis.ExpectedMaxTurns);
+            Assert.IsNotNull(analysis.Explanation);
+            Assert.IsNotNull(analysis.Help);
+        }
+
+        [TestMethod]
+        public void Analize_MustDetectWhenAWordIsCompleted()
+        {
+            State = new GameStateModel("hello");
+            var analysis = Controller.Post(State);
+            Assert.IsTrue(analysis.HasWinner);
+            Assert.AreEqual(1, analysis.Winner);
+            Assert.IsTrue(analysis.Explanation.Contains("exists"));
+        }
+
+        [TestMethod]
+        public void Analize_MustDetectWhenAWordDoesNotExist()
+        {
+            State = new GameStateModel("kkkkk");
+            var analysis = Controller.Post(State);
+            Assert.IsTrue(analysis.HasWinner);
+            Assert.AreEqual(1, analysis.Winner);
+            Assert.IsTrue(analysis.Explanation.Contains("does not exist"));
+        }
+
+        [TestMethod]
+        public void Analize_MustDetectWhoIsGoingToWin()
+        {
+            State = new GameStateModel("hell");
+            var analysis = Controller.Post(State);
+            Assert.IsFalse(analysis.HasWinner);
             Assert.AreEqual(-1, analysis.Winner);
+            Assert.AreEqual(0, analysis.ExpectedWinner);
+        }
+
+        [TestMethod]
+        public void Analize_MustGiveAdvicesToWin()
+        {
+            State = new GameStateModel("hell");
+            var analysis = Controller.Post(State);
+            Assert.IsTrue(analysis.Help.Contains("win going for"));
         }
     }
 }
